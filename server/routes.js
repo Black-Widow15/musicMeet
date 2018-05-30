a8da842eb645fe7b11dc47add995801e15c250cb
Michael Cortez <mijcortez@gmail.com>
2018-05-30 19:08:54 -0400

    routes

--- server/routes.js
+++ server/routes.js
@@ -1,20 +1,27 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/', (req, res, next) => {
+    res.send('<h1>Home page</h1>')
+})
+
 
 router.get('/register', (req, res, next) => {
     // respond to register endpoint
+    res.send('<h1>Register</h1>')
     next()
 })
 
 router.get('/events', (req, res, next) => {
     // respond to events endpoint
+    res.send('<h1>Events</h1>')
     next()
 })
 
 
 router.get('/events', (req, res, next) => {
     // respond to events get
+    res.send('<h1>Events</h1>')
     next()
 })
 router.post('/events', (req, res, next) => {
@@ -24,6 +31,7 @@ router.post('/events', (req, res, next) => {
 
 router.get('/users', (req, res, next) => {
     // respond to get users
+    res.send('<h1>Users</h1>')
     next()
 })
 
@@ -32,7 +40,7 @@ router.post('/users', (req, res, next) => {
     next()
 })
 
-router.get('/error', (req, res, next) => {
+router.get('/', (req, res, next) => {
     res.status(404).send('Error!')
 })
 
