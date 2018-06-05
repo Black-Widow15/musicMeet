c141a43906440ebe6426a3988565fd32fcca8a1d
Mandy Mak <mandymak22@gmail.com>
2018-06-05 16:54:58 -0400

    Bulma-fy User component. Add field to write messages on user's page in User component. Work on rendering events user is attending

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -13,10 +13,10 @@ class User extends React.Component {
       displayName: 'Pancake Jr.',
       musician: true,
       upcomingGigs: [],
-      bio: 'i love music',
-      pinnedTrackUrl: '',
+      bio: 'sometimes they call me flapjack',
       photos: [],
-      eventsAttending: []
+      eventsAttending: ['a', 'b', 'c'],
+      messages: ['this is a message', 'this is also a message'],
     };
   }
 
@@ -29,27 +29,56 @@ class User extends React.Component {
 
   render() {
     return(
-    <div> 
-    <div className="box">
-      <article className="media">
-        <div className="media-left">
-          <figure className="image is-64x64">
-          <img src={this.state.image} alt="Image"/>
-          </figure>
-        </div>
-        <div className="media-content">
-          <div className="content">
-            <p>
-              <strong>{this.state.displayName}</strong> <small>{this.state.name}</small>
-                <br/>
-                {this.state.bio}
-            </p>
-              <EventsAttending attending={this.state.eventsAttending}/>
+    <div>
+      <section className="hero is-warning is-bold">
+        <div className="hero-body">
+          <div className="container">
+          <div className="level-left">
+            <div className="level-item">
+              <figure className="image is-128x128 is-square">
+                <img src={this.state.image}/>
+              </figure>
+            </div>
+            <div className="level-item">
+              <div>
+                <h1 className="title">
+                  {this.state.displayName}
+                </h1><p></p>
+                <h2 className="subtitle">
+                  @{this.state.name}
+                </h2>
+              </div>
+            </div>
+          </div>
           </div>
         </div>
-      </article>
-    </div>
-    <EventsAttending/>
+      </section>
+      <section>
+          <h2><strong>About me</strong></h2>
+          <p>{this.state.bio}</p>
+          <div className="columns">
+            <div className="column">
+              <div className="notification is-primary has-text-centered">
+                my messages
+              </div>
+                <div class="field">
+                  <label class="label">write me a message!</label>
+                  <div class="control">
+                    <input class="input" type="text" placeholder="Start typing here"/>
+                  </div>
+                </div>
+                <div class="control">
+                  <button class="button is-primary">Send!</button>
+                </div>
+                {this.state.messages}
+            </div>
+            <div className="column">
+              <div className="notification is-primary has-text-centered">
+                yo.
+              </div>
+            </div>
+          </div>
+      </section>
     </div>
     )
   }
@@ -57,3 +86,4 @@ class User extends React.Component {
 
 
 export default User;
+// each event that the user is going to will be card
