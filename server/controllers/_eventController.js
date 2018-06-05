5dc4af0187aea8e8b642b50f07a78b8348ba5b85
Michael Cortez <mijcortez@gmail.com>
2018-06-05 16:36:21 -0400

    creatEvent hooked up

--- server/controllers/_eventController.js
+++ server/controllers/_eventController.js
@@ -1,7 +1,7 @@
 const event = require('../database/eventQueries.js')
 
 const saveNewEvent = (req, res) => {
-    console.log(req.body)
+ 
     event.saveEventDB(req.body, (err, result) => {
         if(err){
             console.log('Event not saved into the database')
