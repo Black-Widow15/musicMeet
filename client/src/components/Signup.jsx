b6174277bc13a9988acce0f41647ee295a3999dd
Michael Cortez <mijcortez@gmail.com>
2018-06-05 18:01:12 -0400

    sign up client side

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -10,39 +10,67 @@ class Signup extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            username: '',
-            password: ''
+            name: '',
+            display_name: '',
+            password: '',
+            imgurl: '',
+            email: '',
+            bio: '',
+            musician: false
         }
 
+        this.handleName = this.handleName.bind(this);
         this.handlePassword = this.handlePassword.bind(this);
-        this.handleUsername = this.handleUsername.bind(this);
+        this.handleDisplayName = this.handleDisplayName.bind(this)
+        this.handleProfilePicture = this.handleProfilePicture.bind(this)
+        this.handleEmail = this.handleEmail.bind(this);
+        this.handleBio = this.handleBio.bind(this);
+        
+
         this.handleSubmit = this.handleSubmit.bind(this);
         this.closeSignupModal = this.closeSignupModal.bind(this);
+        
     }
 
-    handleUsername(e){
+    handleName(e){
         this.setState({
-            username: e.target.value,
+            name: e.target.value,
         })
     }
-
     handlePassword(e) {
         this.setState({
             password: e.target.value
         })
     }
-
+    handleDisplayName(e){
+        this.setState({
+            display_name: e.target.value
+        })
+    }
+    handleProfilePicture(e){
+        this.setState({
+            imgurl: e.target.value
+        })
+    }
+    handleEmail(e){
+        this.setState({
+            email: e.target.value
+        })
+    }
+    handleBio(e){
+        this.setState({
+            bio: e.target.value
+        })
+    }
     handleSubmit(e){
         console.log('submitted')
         e.preventDefault()
         // grab username and password from state and send to user schema        
     }
-
     closeSignupModal () {
       let modal = document.getElementById('Signup');
       modal.classList.remove('is-active');
     }
-
     render() {
         return (
             <form onSubmit ={this.handleSubmit}>
@@ -51,19 +79,38 @@ class Signup extends React.Component {
             <div className="modal-content">
                 <div className="box">
                 <div className="field">
-                            <label className="label">Name</label>
-                            <div className="control">
-                                <input className="input" type="text" placeholder="Text input" value = {this.state.username} onChange = {this.handleUsername}/>
-                            </div>
-                        </div>
+                
+                    <label className="label">Name</label>
+                    <div className="control">
+                        <input className="input" type="text" placeholder="Text input" value = {this.state.name} onChange = {this.handleName}/>
+                    </div>
 
-                        <div className="field">
-                            <label className="label">Username</label>
-                            <div className="control has-icons-left has-icons-right">
-                                <input className="input" type="text" placeholder="Text input"  onChange = {this.handlePassword}/>
-                            </div>
-                            </div>
-                    
+                    <label className="label">Display Name</label>
+                    <div className="control">
+                        <input className="input" type="text" placeholder="Text input" value = {this.state.display_name} onChange = {this.handlePassword}/>
+                    </div>
+
+                    <label className="label">Password</label>
+                    <div className="control">
+                        <input className="input" type="text" placeholder="Text input" value = {this.state.password} onChange = {this.handlePassword}/>
+                    </div>
+
+                    <label className="label">Profile Picture</label>
+                    <div className="control">
+                        <input className="input" type="text" placeholder="Text input" value = {this.state.imgurl} onChange = {this.handleProfilePicture}/>
+                    </div>
+
+                    <label className="label">E-mail</label>
+                    <div className="control">
+                        <input className="input" type="text" placeholder="Text input" value = {this.state.email} onChange = {this.handleEmail}/>
+                    </div>
+
+                    <label className="label">Bio</label>
+                    <div className="control">
+                        <input className="input" type="text" placeholder="Text input" value = {this.state.bio} onChange = {this.handleBio}/>
+                    </div>
+                
+                    </div>
                     <button className="button is-link">Submit</button>
                 </div>
             </div>
