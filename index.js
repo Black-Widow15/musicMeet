255012f88a91ba8e20278d6e169d28c48bd80dc9
Mandy Mak <mandymak22@gmail.com>
2018-06-09 04:45:21 -0400

    Set environment variable PORT

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const {router} = require('./server/routes.js')
-const app = express()
-const path = require('path')
-const db = require ('./server/database/connection.js')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const {router} = require('./server/routes.js');
+const app = express();
+const path = require('path');
+const db = require ('./server/database/connection.js');
 
-const port = 8000
+const port = process.env.PORT || 8000;
 
-app.use(express.static(path.join(__dirname + '/client/dist')))
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-
-
-app.use(bodyParser.json())
-app.use(cors())
-app.use(router)
+app.use(bodyParser.json());
+app.use(cors());
+app.use(router);
 
 app.listen(port, () => {
-    console.log(`Magic happens on port ${port}`)
-})
+  console.log(`Magic happens on port ${port}`);
+});
